ea578819ebacfe9eed70cdfedbe55f2b7af3f94e
safouanebazzi <safouanebazzi@gmail.com>
2024-11-05 11:15:00 +0100

    Update tp_2.py

--- tp_2.py
+++ tp_2.py
@@ -1,13 +1,13 @@
-exercice 4:
+#exercise 4:
 def repete_caractere(c, n):
     return c * n
 print(repete_caractere('a', 5))  # Affiche 'aaaaa'
-exercice 5:
+#exercise 5:
 def repete_deux_caracteres(c, n, d, m):
     return c * n + d * m
 
 print(repete_deux_caracteres('a', 3, 'b', 4))  
-exercice 6:
+#exercise 6:
 def affiche_n_lignes(ch, n):
     for _ in range(n):
         print(ch)
@@ -18,13 +18,13 @@ def affiche_n_lignes_numerote(ch, n):
         print(f"{i}. {ch}")
 
 affiche_n_lignes_numerote('Bonjour', 3)
-exercice 7:
+#exercise 7:
 def triangle_caractere(c, n):
     for i in range(1, n + 1):
         print(c * i)
 
 triangle_caractere('*', 4)
-exercice 8:
+#exercise 8:
 def premiere_occ(ch, c):
     for i, char in enumerate(ch):
         if char == c:
@@ -33,19 +33,19 @@ def premiere_occ(ch, c):
 
 print(premiere_occ('aabcada', 'a'))  
 print(premiere_occ('abcd', 'e'))    
-exercice 9:
+#exercise 9:
 def nb_occurrence(ch, c):
     return ch.count(c)
 
 print(nb_occurrence('aabcada', 'a'))  
 print(nb_occurrence('abcd', 'e'))     
-exercice 10:
+#exercise 10:
 def sous_chaine(ch1, ch2):
     return ch1 in ch2 or ch2 in ch1
 
 print(sous_chaine('abc', 'abcd'))  
 print(sous_chaine('abc', 'xyz'))  
-exercice 11:
+#exercise 11:
 ch = ''
 for k in range(1, 5):
     if k % 2 == 1:
@@ -61,7 +61,7 @@ for k in range(4):
     else:
         ch += 'b'
 print(ch)
-exercice 12:
+#exercise 12:
 def triple_six(ch):
     return '666' in ch
 
@@ -77,17 +77,17 @@ def triple_six_exact(ch):
         if ch[k:k+3] == '666' and ch[k-1] != '6' and ch[k+3] != '6':
             return True
     return False
-exercice 13:
+#exercise 13:
 def palindrome(ch):
     return ch == ch[::-1]
 
 print(palindrome('kayak')) 
-exercice 19:
+#exercise 19:
 def indices(ch, c):
     return [i for i, char in enumerate(ch) if char == c]
 
 print(indices('abbacccaa', 'a')) 
-exercice 20:
+#exercise 20:
 def sous_liste(L1, L2):
     n, m = len(L1), len(L2)
     for i in range(n - m + 1):
@@ -96,13 +96,13 @@ def sous_liste(L1, L2):
     return False
 
 print(sous_liste([1, 2, 3, 4, 5], [3, 4])) 
-exercice 21:
+#exercise 21:
 def tous_positifs(L):
     return all(x >= 0 for x in L)
 
 print(tous_positifs([1, 2, 3, 0]))  
 print(tous_positifs([-1, 2, 3]))  
-exercice 22:
+#exercise 22:
 def positifs(L):
     return sum(1 for x in L if x >= 0)
 
@@ -119,7 +119,7 @@ def dix_vingt(L):
     return all(10 <= x <= 20 for x in L)
 
 print(dix_vingt([15, 12, 20]))  
-exercice 23:
+#exercise 23:
 def croissante(L):
     n = len(L)
     for k in range(n - 1):
@@ -132,7 +132,7 @@ def monotone(L):
 
 print(monotone([1, 2, 3]))   
 print(monotone([3, 2, 1]))  
-exercice 25:
+#exercise 25:
 def suite(n):
     u = 1
     L = [u]
@@ -142,7 +142,7 @@ def suite(n):
     return L
 
 print(suite(5))  
-exercice 26:
+#exercise 26:
 def valeur_absolue_liste(L):
     for i in range(len(L)):
         L[i] = abs(L[i])
@@ -150,7 +150,7 @@ def valeur_absolue_liste(L):
 L = [-1, -2, 3]
 valeur_absolue_liste(L)
 print(L)  
-exercice 27:
+#exercise 27:
 def E_et_sigma(X, P):
     E = sum(x * p for x, p in zip(X, P))
     variance = sum(p * (x - E) ** 2 for x, p in zip(X, P))
@@ -158,16 +158,16 @@ def E_et_sigma(X, P):
     return E, sigma
 
 print(E_et_sigma([1, 2, 3], [0.2, 0.5, 0.3]))
-exercice 30:
+#exercise 30:
 import copy
 M = [[0, 0, 0] for _ in range(3)]
 N = copy.deepcopy(M)
-exercice 32:
+#exercise 32:
 from copy import deepcopy
 
 def valeur_absolue_liste_2(L):
     return [abs(e) for e in L]
-exercice 33:
+#exercise 33:
 def ajout(L, o):
     return L + [o]
 
